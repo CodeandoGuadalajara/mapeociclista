9d2a3e168c93cad61fb903ccc3eff898947418ca
Hans <hans@hackergarage.mx>
2016-06-16 12:25:32 -0500

    working dictionary 1.0

--- scraper.js
+++ scraper.js
@@ -7,20 +7,33 @@ streamOfTweets = new TwitterPosts({
 });
 
 streamOfTweets.on('readable', function() {
-  var time, tweet, dictionary;
+  var time, tweet, dictionary, newdict;
   tweet = streamOfTweets.read();
   time = new Date(tweet.time * 1000);
   dictionary = "choque";
+  newdict = ["bici","bicicleta"," ciclista", "ciclo estación"];
 
-  if(tweet.text.indexOf(dictionary) != -1){
-    /*console.log(tweet.text);*/
+  for (var i = 0; i < newdict.length; i++) {
+    if(tweet.text.indexOf(newdict[i]) != -1){
+      console.log([
+        "tweet del día: ",
+        time.toLocaleString(),
+        " tweet:, ",
+        tweet.text,
+      ].join(''));
+    }
+  }
+
+
+/*  if(tweet.text.indexOf(dictionary) != -1){
+
     console.log([
       "tweet del día: ",
       time.toLocaleDateString(),
       " tweet:, ",
       tweet.text,
     ].join(''));
-  }
+  }*/
 
 
 
